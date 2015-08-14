0ddb3f343d3709713b165b4a51eab34d3c47d573
perry <perry.r.eising@gmail.com>
2015-08-14 10:12:08 -0700

    Add test to confirm that string input is returned as string. Test passes.

--- src/test/wordPuzzTest.java
+++ src/test/wordPuzzTest.java
@@ -5,20 +5,10 @@ public class wordPuzzTest {
 
   @Test
   public void InputStringOutputsAString_istrue() { //maybe come back to this?
-    wordPuzz testWordPuzz = new CoinCombo();
-    Integer testInput = 5;
-    String outputString = testCoinCombo.createChange(testInput);
+    wordPuzz testWordPuzz = new wordPuzz();
+    String testInput = "String";
+    String outputString = testCoinCombo.createOutput(testInput);
     assertEquals(true, outputString instanceof String);
-  }
-
-
-  @Test
-  public void InputStringHasCorrectOutput_istrue() { //maybe come back to this?
-    CoinCombo testCoinCombo = new CoinCombo();
-    Integer testInput = 5;
-    String outputString = "You have used 0 quarter(s), 0 dime(s), 1 nickel(s), and 0 penny/pennies. Enjoy!";
-    String otherOutputString  = testCoinCombo.createChange(testInput);
-    assertEquals(true, true);
   }
 
 }
